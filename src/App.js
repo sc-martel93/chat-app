f76bdc045ff8bad05d4d738bad7660da9356b59a
sc-martel93 <80806216+sc-martel93@users.noreply.github.com>
2021-05-02 18:02:47 -0400

    Initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 import './App.css';
 
+
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAWyvUoCqJIFfQ7emDC8lowSinAmCrgH6w",
+  authDomain: "chat-app-cd6c7.firebaseapp.com",
+  projectId: "chat-app-cd6c7",
+  storageBucket: "chat-app-cd6c7.appspot.com",
+  messagingSenderId: "49799691310",
+  appId: "1:49799691310:web:1581c7e1c7424a4a95f3bf",
+  measurementId: "G-P7GYL1KC3K"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
+
 function App() {
   return (
     <div className="App">
-      <h1>Chat-App</h1>
+      <header>
+
+      </header>
+      <section>
+
+      </section>
     </div>
   );
 }
